d87f647d9c24d2c425e4b7843776ae06d4ab2f26
Golyb4ik <only_game2@mail.ru>
2023-03-05 20:42:43 +0300

    zadacha Stream

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
-      Supplier<Integer>supplier=new Supplier<Integer>(){
-          @Override
-          public Integer get(){
-              return (int)(Math.random()*100);
-          }
-      };
-      Supplier<Integer>supplier1=()->(int)(Math.random()*100);
+
     }
 
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? extends T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            list.sort((Comparator<? super T>) order);
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
+    }
+
+    public static void findCountEvenNums(List<Integer> list) {
+        Stream<Integer> stream = list.stream();
+        Predicate<Integer> predicate = (n) -> (n % 2) == 0;
+        Stream<Integer> resStream = stream.filter(predicate);
+        System.out.println("Кол.четных чисел" + resStream.count());
+    }
 }
