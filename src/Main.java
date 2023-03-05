736f685b58df1d45a16ef78f3daf26920596569c
Golyb4ik <only_game2@mail.ru>
2023-03-05 15:51:58 +0300

    zadacha 4

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
-       Function<Double,Long>function=new Function<Double, Long>() {
-           @Override
-           public Long apply(Double aDouble) {
-               return Math.round(aDouble);
-           }
-       };
-        Function<Double,Long>function1=y ->Math.round(y);
+      Supplier<Integer>supplier=new Supplier<Integer>(){
+          @Override
+          public Integer get(){
+              return (int)(Math.random()*100);
+          }
+      };
+      Supplier<Integer>supplier1=()->(int)(Math.random()*100);
     }
 
 }
